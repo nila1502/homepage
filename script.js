1e5f5eb4deef297f72b7d6cfc6d3383812c8a89a
nila1502 <113190409+nila1502@users.noreply.github.com>
2022-10-01 08:29:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 var images;
 
 
-images = ['https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'https://images.pexels.com/photos/1181244/pexels-photo-1181244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'];
-let element_img = document.getElementById('img');
-element_img.setAttribute("src", images[0]);
 
+document.getElementById('start').addEventListener('click', (event) => {
+  images = ['https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'https://images.pexels.com/photos/1181244/pexels-photo-1181244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'];
+  let element_image = document.getElementById('image');
+  element_image.setAttribute("src", images[0]);
+
+});
 
 document.getElementById('previous').addEventListener('click', (event) => {
-  let element_img2 = document.getElementById('img');
+  let element_image2 = document.getElementById('image');
   images.unshift(images.slice(-1)[0]);
-  element_img2.setAttribute("src", images.pop());
+  element_image2.setAttribute("src", images.pop());
 
 });
 
 document.getElementById('next').addEventListener('click', (event) => {
-  let element_img3 = document.getElementById('img');
+  let element_image3 = document.getElementById('image');
   images.push(images[0]);
-  element_img3.setAttribute("src", images.shift());
+  element_image3.setAttribute("src", images.shift());
 
 });
